7a370ab4baca5de39c98db8af8d56ec3a04f59d8
jinioh88 <jinioh88@gmail.com>
2020-06-08 09:51:21 +0900

    싱글턴 패턴 적용 전

---
+++ src/main/java/com/example/java_design/ch6/Main.java
@@ -0,0 +1,13 @@
+package com.example.java_design.ch6;
+
+public class Main {
+    private static final int USER_NUM = 5;
+
+    public static void main(String[] args) {
+        User[] users = new User[USER_NUM];
+        for(int i = 0; i < USER_NUM; i++) {
+            users[i] = new User((i+1) + "-user");
+            users[i].print();
+        }
+    }
+}
---
+++ src/main/java/com/example/java_design/ch6/Printer.java
@@ -0,0 +1,19 @@
+package com.example.java_design.ch6;
+
+public class Printer {
+    private static Printer printer = null;
+
+    private Printer() {}
+
+    public static Printer getPrinter() {
+        if(printer == null) {
+            printer  = new Printer();
+        }
+
+        return printer;
+    }
+
+    public void print(String str) {
+        System.out.println(str);
+    }
+}
